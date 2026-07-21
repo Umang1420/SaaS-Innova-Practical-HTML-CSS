a0aad61bdb860cf14a0bcb433ee613660a1e6146
Umang1420 <umangchaudhari670@gmail.com>
2026-07-21 17:27:02 +0530

    Quiz App

--- JS/Extra Tasks/Quiz app/script.js
+++ JS/Extra Tasks/Quiz app/script.js
@@ -1,7 +1,3 @@
-let sub = document.getElementById("su");
-
-
-let correctcount = 0;
 let questions = [{
     "id": "01",
     "question":"Q1- Capital of Australia?",
@@ -44,73 +40,7 @@ let questions = [{
     "ans":"B"
 }
 ];
-// async function* genrator(array){
-//     for(let question of array){
-        
-//         yield question;
-//     }
-// }
-
-// let nextq = genrator(questions);
-
-// async function  nextquestion() {
-//     let result = await nextq.next();
- 
-        
-    
-//     if(!result.done){
-//         document.getElementById("box").innerHTML =JSON.stringify(re);
-//     }else{
-//         console.log("no more values");
-//     }
-// }
-//    let re = ""
-//          re += `<div class="question-box">
-//                      <div class="questions">
-//                          <p>${result[i].question}</p><p id="cou">${correctcount}</p>                     </div>
-//                      <div class="options">
-//                          <form>
-//                             <input type="radio" id="a" name="a" value="optionA">
-//                             <label for="html">${result[i].A}</label><br>
-//                             <input type="radio" id="b" name="a" value="optionB">
-//                             <label for="css">${result[i].B}</label><br>
-//                             <input type="radio" id="c" name="a" value="optionC">
-//                             <label for="javascript">${result[i].C}</label><br>
-//                             <input type="radio" id="d" name="a" value="optionD">
-//                             <label for="javascript">${result[i].D}</label>
-//                         </form>
-//                     </div>
-//                     <div class="btn">
-//                         <button type="submit" id="su">Submit</button>
-//                     </div>
-//              </div>`
-
-// function dis(){
-//     let qu = ""
-//     for(let i=0; i<questions.length; i++){
-//         qu += `<div class="question-box">
-//                     <div class="questions">
-//                         <p>${questions[i].question}</p><p id="cou">${correctcount}</p>
-//                     </div>
-//                     <div class="options">
-//                         <form>
-//                             <input type="radio" id="a" name="a" value="optionA">
-//                             <label for="html">${questions[i].A}</label><br>
-//                             <input type="radio" id="b" name="a" value="optionB">
-//                             <label for="css">${questions[i].B}</label><br>
-//                             <input type="radio" id="c" name="a" value="optionC">
-//                             <label for="javascript">${questions[i].C}</label><br>
-//                             <input type="radio" id="d" name="a" value="optionD">
-//                             <label for="javascript">${questions[i].D}</label>
-//                         </form>
-//                     </div>
-//                     <div class="btn">
-//                         <button type="submit" id="su">Submit</button>
-//                     </div>
-//                 </div>`
-//     }
-//     document.getElementById("box").innerHTML = qu;
-// }
+let correctcount = 0;
 
 function correct(){
     correctcount = correctcount+1; 
@@ -119,35 +49,53 @@ function correct(){
 }
 
 
-let i = 0;
+
+let a = 0;
+function index(){
+    a++
+    console.log(a);
+}
 function dis(){
     let qu = ""
-   
-    for(i<questions.length; i++;)
+    let box = document.getElementById("box");
+    
         qu += `<div class="question-box">
                     <div class="questions">
-                        <p>${questions[i].question}</p><p id="cou">${correctcount}</p>
+                        <p>${questions[a].question}</p><p id="cou">${a+1}</p>
                     </div>
                     <div class="options">
-                        <form>
-                            <input type="radio" id="a" name="a" value="optionA">
-                            <label for="html">${questions[i].A}</label><br>
-                            <input type="radio" id="b" name="a" value="optionB">
-                            <label for="css">${questions[i].B}</label><br>
-                            <input type="radio" id="c" name="a" value="optionC">
-                            <label for="javascript">${questions[i].C}</label><br>
-                            <input type="radio" id="d" name="a" value="optionD">
-                            <label for="javascript">${questions[i].D}</label>
+                        <form id="quizForm">
+                            
+                            <input type="radio" id="a" name="quizOption" value="A">
+                            <label for="a">${questions[a].A}</label><br>
+                            
+                            <input type="radio" id="b" name="quizOption" value="B">
+                            <label for="b">${questions[a].B}</label><br>
+                            
+                            <input type="radio" id="c" name="quizOption" value="C">
+                            <label for="c">${questions[a].C}</label><br>
+                            
+                            <input type="radio" id="d" name="quizOption" value="D">
+                            <label for="d">${questions[a].D}</label>
                         </form>
                     </div>
-                    <div class="btn">
-                        <button type="submit" id="see">Submit</button>
-                    </div>
+                   
                 </div>`
-    document.getElementById("box").innerHTML = qu;
-    i = i + 1;
-    console.log(i);
+        
+        document.getElementById("box1").innerHTML = qu;
+        
 }
 dis();
 
+let see = document.getElementById("see");
+see.addEventListener("click",dis);
 
+function check(){
+    let selected = document.querySelector('input[name="quizOption"]:checked');
+   
+        if(selected === questions[a].ans){
+            console.log("done");
+        }else{
+            console.log("issue");
+        }
+}
