e4989d1fef9dcea5785440c7cbfe381484bee6da
Umang <umangchaudhari670@gmail.com>
2026-07-26 15:59:31 +0530

    Interactive Photo Gallary

--- JS/Extra Tasks/Interactive Photo Gallary/script.js
+++ JS/Extra Tasks/Interactive Photo Gallary/script.js
@@ -72,13 +72,3 @@ close.addEventListener("click",function(){
     modal.style.display="none";
 
 });
-
-window.addEventListener("click",function(e){
-
-    if(e.target==modal){
-
-        modal.style.display="none";
-
-    }
-
-});
